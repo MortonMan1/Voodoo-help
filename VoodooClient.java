995ca5cb7ddf1c43a833067f734e9b0a81295d6a
Harrison Jones <harrisonhjones@gmail.com>
2015-01-02 16:49:29 -0500

    Changed from UDP datagrams to TCP sockets as needed

--- VoodooClient.java
+++ VoodooClient.java
@@ -3,27 +3,40 @@ import java.io.*;
 
 public class VoodooClient
 {
-	public static void main(String args[]) throws IOException
-	{
-		int serverPort = 48879;
-		String host = "SparkIP";
-		
-		
-		// get a datagram socket
-	    DatagramSocket socket = new DatagramSocket();
-	
-	        // send request
-	    byte[] Text = {0x00, 0x07, 0x01};
-	    InetAddress address = InetAddress.getByName(host);
-	    DatagramPacket packet = new DatagramPacket(Text, Text.length, address, serverPort);
-	    socket.send(packet);
-	    System.out.println("Sent initialiser");
-	    
-	    byte[] Text2 = {0x01, 0x07, 0x01};
-	    DatagramPacket packet2 = new DatagramPacket(Text2, Text2.length, address, serverPort);
-	    socket.send(packet2);
-	    System.out.println("Sent ON");
-	
-	    socket.close();
-	}
+	public static void main(String[] args) {
+        // TODO code application logic here
+        int port = 48879;		// REPLACE WITH YOUR CORE's PORT
+        String host = "10.0.0.31";	// REPLACE WITH YOUR CORE'S IP
+       
+        try {
+            Socket clientSocket = new Socket(host, port);
+            DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            
+            byte[] output7 = {0x00, 0x07, 0x01};   // Set pin 7 to OUTPUT
+            byte[] high7 = {0x01, 0x07, 0x01};  // Set pin 7 to HIGH
+            byte[] low7 = {0x01, 0x07, 0x00};  // Set pin 7 to HIGH
+            
+            outToServer.write(output7);
+            System.out.println("Sent initialiser");
+
+            for(int i = 0; i<10; i++)
+            {
+                outToServer.write(high7);
+                System.out.println("Sent ON");
+                Thread.sleep(1000);
+                outToServer.write(low7);
+                System.out.println("Sent OFF");
+                Thread.sleep(1000);
+            }
+            
+            clientSocket.close();
+        } catch (SocketException ex) {
+            Logger.getLogger(VoodooSparkClient.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(VoodooSparkClient.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(VoodooSparkClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }	
